59d279a565548cb71ddfc8154c7f39dacfac2565
DuyKaito9140 <dcduya1nd2018@gmail.com>
2021-05-15 02:08:16 +0700

    15/5/2021

---
+++ Controllers/PromotionController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HeThongQuanLyDatVeMayBay.Models;
+using PagedList;
+
+namespace HeThongQuanLyDatVeMayBay.Controllers
+{
+    public class PromotionController : Controller
+    {
+        DBEntities_QLHeThongDatVeMayBay db = new DBEntities_QLHeThongDatVeMayBay();
+        PromotionModel promo = new PromotionModel();
+        public ActionResult Index(int? page, string mysearch)
+        {
+            if (page == null) page = 1;
+            var links = new List<KHUYENMAI>();
+            if (!String.IsNullOrEmpty(mysearch))
+            {
+                links = (from l in db.KHUYENMAIs
+                         select l).OrderBy(x => x.idKhuyenMai).Where(m => m.MaKhuyenMai.Contains(mysearch)
+                         || m.NgayBatDauKM.Value.ToString().Contains(mysearch) || m.NgayKetThucKM.Value.ToString().Contains(mysearch)
+                         || m.GiaTriKM.ToString().Contains(mysearch) || m.ChiTietMoTaKM.Contains(mysearch)).ToList();
+            }
+            else
+            {
+                links = (from l in db.KHUYENMAIs
+                         select l).OrderBy(x => x.idKhuyenMai).ToList();
+            }
+            int pageSize = 8;
+
+            int pageNumber = (page ?? 1);
+            return View(links.ToPagedList(pageNumber, pageSize));
+        }
+        [HttpPost]
+        public ActionResult Index(string mysearch)
+        {
+            mysearch = "";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(KHUYENMAI km)
+        {
+            promo.them_khuyenmai(km);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(string id)
+        {
+            return View(promo.layOnekhuyenmai(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(KHUYENMAI km)
+        {
+            promo.sua_khuyenmai(km);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string id)
+        {
+            promo.xoa_khuyenmai(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/PromotionModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeThongQuanLyDatVeMayBay.Models
+{
+    public class PromotionModel
+    {
+        DBEntities_QLHeThongDatVeMayBay db = new DBEntities_QLHeThongDatVeMayBay();
+        public IEnumerable<KHUYENMAI> ListMayBay()
+        {
+            return db.KHUYENMAIs.ToList();
+        }
+
+        public KHUYENMAI layOnekhuyenmai(string id)
+        {
+            return db.KHUYENMAIs.First(m => m.idKhuyenMai == id);
+        }
+
+        public void them_khuyenmai(KHUYENMAI km)
+        {
+            string idkm = Content.RandomString(8);
+            km.idKhuyenMai = idkm;
+            db.KHUYENMAIs.Add(km);
+            db.SaveChanges();
+        }
+
+        public void sua_khuyenmai(KHUYENMAI km)
+        {
+            KHUYENMAI x = layOnekhuyenmai(km.idKhuyenMai);
+            x.MaKhuyenMai = km.MaKhuyenMai;
+            x.NgayBatDauKM = km.NgayBatDauKM;
+            x.NgayKetThucKM = km.NgayKetThucKM;
+            x.GiaTriKM = km.GiaTriKM;
+            x.ChiTietMoTaKM = km.ChiTietMoTaKM;
+            db.SaveChanges();
+        }
+
+        public void xoa_khuyenmai(string id)
+        {
+            KHUYENMAI x = layOnekhuyenmai(id);
+            db.KHUYENMAIs.Remove(x);
+            db.SaveChanges();
+        }
+    }
+}
