8807e8d0063a2777890c65aaf5873e5b649a07c9
DuyKaito9140 <dcduya1nd2018@gmail.com>
2021-05-19 16:34:02 +0700

    Version 04 MVC ASP.NET

---
+++ HeThongQuanLyDatVeMayBay/Controllers/RoleController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HeThongQuanLyDatVeMayBay.Models;
+using PagedList;
+
+namespace HeThongQuanLyDatVeMayBay.Controllers
+{
+    public class RoleController : Controller
+    {
+        DBEntities_QLHeThongDatVeMayBay db = new DBEntities_QLHeThongDatVeMayBay();
+        RoleModel rm = new RoleModel();
+        public ActionResult Index(int? page, string mysearch)
+        {
+            if (page == null) page = 1;
+            var links = new List<LOAIQUYEN>();
+            if (!String.IsNullOrEmpty(mysearch))
+            {
+                links = (from l in db.LOAIQUYENs
+                         select l).OrderBy(x => x.idQuyen).Where(m => m.TenQuyen.Contains(mysearch)).ToList();
+            }
+            else
+            {
+                links = (from l in db.LOAIQUYENs
+                         select l).OrderBy(x => x.idQuyen).ToList();
+            }
+            int pageSize = 8;
+
+            int pageNumber = (page ?? 1);
+            return View(links.ToPagedList(pageNumber, pageSize));
+        }
+        [HttpPost]
+        public ActionResult Index(string mysearch)
+        {
+            mysearch = "";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(LOAIQUYEN q)
+        {
+            rm.them_quyen(q);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(string id)
+        {
+            return View(rm.layOnequyen(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(LOAIQUYEN q)
+        {
+            rm.sua_quyen(q);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string id)
+        {
+            rm.xoa_quyen(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HeThongQuanLyDatVeMayBay/Models/RoleModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeThongQuanLyDatVeMayBay.Models
+{
+    public class RoleModel
+    {
+        DBEntities_QLHeThongDatVeMayBay db = new DBEntities_QLHeThongDatVeMayBay();
+        public IEnumerable<LOAIQUYEN> Listquyen()
+        {
+            return db.LOAIQUYENs.ToList();
+        }
+
+        public LOAIQUYEN layOnequyen(string id)
+        {
+            return db.LOAIQUYENs.First(m => m.idQuyen == id);
+        }
+
+        public void them_quyen(LOAIQUYEN q)
+        {
+            string idq = Content.RandomString(8);
+            q.idQuyen = idq;
+            db.LOAIQUYENs.Add(q);
+            db.SaveChanges();
+        }
+
+        public void sua_quyen(LOAIQUYEN mb)
+        {
+            LOAIQUYEN x = layOnequyen(mb.idQuyen);
+            x.TenQuyen = mb.TenQuyen;
+            db.SaveChanges();
+        }
+
+        public void xoa_quyen(string id)
+        {
+            LOAIQUYEN x = layOnequyen(id);
+            db.LOAIQUYENs.Remove(x);
+            db.SaveChanges();
+        }
+    }
+}
